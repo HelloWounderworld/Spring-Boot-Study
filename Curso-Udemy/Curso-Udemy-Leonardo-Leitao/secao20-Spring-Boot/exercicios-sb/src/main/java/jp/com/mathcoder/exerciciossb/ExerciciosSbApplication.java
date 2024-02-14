9d770b1e9e4a649511b74c3155e5c678c92ec6fd
Leonardo Takashi Teramatsu <vercel@buudigital.com>
2024-02-13 21:33:40 -0300

    Atualizand oa minha nota

---
+++ Curso-Udemy/Curso-Udemy-Leonardo-Leitao/secao20-Spring-Boot/exercicios-sb/src/main/java/jp/com/mathcoder/exerciciossb/ExerciciosSbApplication.java
@@ -0,0 +1,13 @@
+package jp.com.mathcoder.exerciciossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosSbApplication.class, args);
+	}
+
+}
---
+++ Curso-Udemy/Curso-Udemy-Leonardo-Leitao/secao20-Spring-Boot/exercicios-sb/src/test/java/jp/com/mathcoder/exerciciossb/ExerciciosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.com.mathcoder.exerciciossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
